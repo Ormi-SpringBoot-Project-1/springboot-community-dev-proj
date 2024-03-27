aee1e2256788f8c2d5097cd459b2e69e98490098
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-27 20:22:29 +0900

    Feat : issue#5 특정 회원의 닉네임 조회 API 개발
    Path Variable로 특정 회원의 닉네임을 받아 결과 반환
    Controller에서 RestController로 변경

--- src/main/java/com/springbootcommunitydevproj/controller/UserManagementController.java
+++ src/main/java/com/springbootcommunitydevproj/controller/UserManagementController.java
@@ -5,12 +5,12 @@ import com.springbootcommunitydevproj.service.UserManagementService;
 import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @AllArgsConstructor
 public class UserManagementController {
 
@@ -20,11 +20,20 @@ public class UserManagementController {
     // Query String의 page 변수를 통해 페이징됩니다.
     // 한 페이지 당 최대 30개의 결과를 가져옵니다.
     @GetMapping("/api/admin/user_list")
-    @ResponseBody
     public ResponseEntity<List<UserManagementInfoDto>> getAllUserManagementInfo(
         @RequestParam(name = "page", defaultValue = "1") Integer page) {
         return ResponseEntity.ok()
             .header("Content-Type", "application/json")
             .body(userManagementService.getAllUserManagementInfo(page));
+    }
+
+    // Admin 페이지의 회원 관리 페이지에서 특정 회원의 닉네임을 검색했을 때 결과를 가져옵니다.
+    // 리스트 형태로 Response를 반환합니다.
+    @GetMapping("/api/admin/user/{nickname}")
+    public ResponseEntity<List<UserManagementInfoDto>> getUserManagementInfoByNickname(
+        @PathVariable(name = "nickname") String nickname) {
+        return ResponseEntity.ok()
+            .header("Content-Type", "application/json")
+            .body(userManagementService.getUserManagementInfoByNickname(nickname));
     }
 }
