e86eaa3c3aa935d7d1817f1393972fe7696480a3
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-27 20:20:42 +0900

    Feat : issue#5 특정 회원의 닉네임 조회 Service 메소드 개발

--- src/main/java/com/springbootcommunitydevproj/service/UserManagementService.java
+++ src/main/java/com/springbootcommunitydevproj/service/UserManagementService.java
@@ -16,4 +16,10 @@ public class UserManagementService {
     public List<UserManagementInfoDto> getAllUserManagementInfo(Integer page) {
         return userManagementRepository.findAllUserManagementInfo((page - 1) * 30);
     }
+
+    // 회원의 닉네임을 기준으로 해당 회원 정보를 조회합니다.
+    // 조회 결과가 없으면 빈 리스트를 반환합니다.
+    public List<UserManagementInfoDto> getUserManagementInfoByNickname(String nickname) {
+        return userManagementRepository.findByNickname(nickname);
+    }
 }
