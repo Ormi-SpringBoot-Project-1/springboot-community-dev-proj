e3a2a4a9aa42c1ac68a8fcfa5da6b8f996ff96f3
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-27 22:34:40 +0900

    Feat : issue#5 특정 회원의 등급 변경 기능 개발

--- src/main/java/com/springbootcommunitydevproj/repository/UserManagementRepository.java
+++ src/main/java/com/springbootcommunitydevproj/repository/UserManagementRepository.java
@@ -4,9 +4,13 @@ import com.springbootcommunitydevproj.domain.User;
 import com.springbootcommunitydevproj.dto.UserManagementInfoDto;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
+@Repository
 public interface UserManagementRepository extends JpaRepository<User, Integer> {
 
     // user 테이블, level 테이블, post 테이블에서 회원 ID, 닉네임, 회원 등급, 총 게시물 수를 조회해 가져옵니다.
@@ -21,4 +25,14 @@ public interface UserManagementRepository extends JpaRepository<User, Integer> {
         + "from (user as user left outer join level as level on user.level_id = level.level_id) left outer join post as post on user.user_id = post.user_id "
         + "where user.is_admin = 0 and :nickname = user.nickname group by user.user_id", nativeQuery = true)
     List<UserManagementInfoDto> findByNickname(@Param("nickname") String nickname);
+
+    // user_id를 통해 회원을 특정하고 그 회원의 등급을 level로 조정합니다.
+    // 쿼리 결과를 JPA 영속성 컨텍스트를 거치지 않고 바로 DB에 반영합니다.
+    // 이 작업은 Atomic하게 동작합니다.
+    @Query(value = "update `user` as U, (select * from `level` where `level`.level = :level) as L "
+        + "set U.level_id = L.level_id "
+        + "where U.user_id = :userId", nativeQuery = true)
+    @Modifying
+    @Transactional
+    Integer changeUserLevel(@Param("userId") Integer userId, @Param("level") Integer level);
 }
