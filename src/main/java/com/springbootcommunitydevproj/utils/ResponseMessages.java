531e88bb81ed84be2c05cc345942fad15860547c
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-27 22:40:52 +0900

    Feat : issue#5 Response용 메시지 Util 클래스 작성

---
+++ src/main/java/com/springbootcommunitydevproj/utils/ResponseMessages.java
@@ -0,0 +1,11 @@
+package com.springbootcommunitydevproj.utils;
+
+/**
+ * Response용 메시지입니다.
+ */
+public class ResponseMessages {
+
+    public static final String CHANGE_LEVEL_SUCCESS = "등급이 정상적으로 변경되었습니다.";
+    public static final String CHANGE_LEVEL_FAIL = "등급 변경에 실패했습니다.";
+    public static final String INVALID_LEVEL = "유효하지 않은 등급입니다.";
+}
