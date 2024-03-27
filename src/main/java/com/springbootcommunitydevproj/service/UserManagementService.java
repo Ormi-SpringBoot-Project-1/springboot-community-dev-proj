c0883bcea0bf89d523b683413953fa1c252ba2ff
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-27 22:36:22 +0900

    Feat : issue#5 특정 회원의 등급 변경 Service 메소드 개발
    Argument에 대한 데이터 유효성 검사 기능
    결과 메시지 반환

--- src/main/java/com/springbootcommunitydevproj/service/UserManagementService.java
+++ src/main/java/com/springbootcommunitydevproj/service/UserManagementService.java
@@ -2,7 +2,9 @@ package com.springbootcommunitydevproj.service;
 
 import com.springbootcommunitydevproj.dto.UserManagementInfoDto;
 import com.springbootcommunitydevproj.repository.UserManagementRepository;
+import com.springbootcommunitydevproj.utils.ResponseMessages;
 import java.util.List;
+import java.util.Map;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -21,5 +23,23 @@ public class UserManagementService {
     // 조회 결과가 없으면 빈 리스트를 반환합니다.
     public List<UserManagementInfoDto> getUserManagementInfoByNickname(String nickname) {
         return userManagementRepository.findByNickname(nickname);
+    }
+
+    // 특정 회원의 등급을 변경합니다.
+    public String changeUserLevel(Map<String, Integer> request) {
+        // request에 회원의 user_id가 없거나 변경할 level이 없으면 변경 실패 메시지를 반환합니다.
+        if (!request.containsKey("user_id") || !request.containsKey("level")) {
+            return ResponseMessages.CHANGE_LEVEL_FAIL;
+        }
+
+        // 만약 변경하고자 하는 level이 올바른 범위(5 ~ 1)를 벗어나면 유효하지 않은 등급 메시지를 반환합니다.
+        if (request.get("level") <= 0 || 5 < request.get("level")) {
+            return ResponseMessages.INVALID_LEVEL;
+        }
+
+        Integer result =  userManagementRepository.changeUserLevel(request.get("user_id"), request.get("level"));
+
+        // 쿼리 실행 결과에 따라 성공, 실패 메시지를 반환합니다.
+        return result.equals(1) ? ResponseMessages.CHANGE_LEVEL_SUCCESS : ResponseMessages.CHANGE_LEVEL_FAIL;
     }
 }
