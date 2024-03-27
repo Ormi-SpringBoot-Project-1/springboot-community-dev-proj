0471c1b7440915da381dcd483e001cff84851aee
Imlay16 <artisboom@naver.com>
2024-03-27 15:01:21 +0900

    Entities edited

--- src/main/java/com/springbootcommunitydevproj/controller/PostController.java
+++ src/main/java/com/springbootcommunitydevproj/controller/PostController.java
@@ -5,12 +5,12 @@ import com.springbootcommunitydevproj.dto.PostResponse;
 import com.springbootcommunitydevproj.service.PostService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class PostController {
     private final PostService postService;
 
