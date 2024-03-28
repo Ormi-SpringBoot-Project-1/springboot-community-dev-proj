aefa272751d7d71975a49bd20c9ee6da51245a42
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-28 13:16:48 +0900

    Feat : issue#5 특정 회원 가입 제한 API의 Request DTO 작성

---
+++ src/main/java/com/springbootcommunitydevproj/dto/SetUserToBlockedUserRequest.java
@@ -0,0 +1,15 @@
+package com.springbootcommunitydevproj.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetUserToBlockedUserRequest {
+
+    private Integer userId;
+}
