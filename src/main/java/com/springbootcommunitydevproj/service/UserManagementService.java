49cd8eba7f7bfae846e2015cbb7a70f82aabfa80
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-28 13:17:36 +0900

    Feat : issue#5 특정 회원 가입 제한 Service 메소드 작성

--- src/main/java/com/springbootcommunitydevproj/service/UserManagementService.java
+++ src/main/java/com/springbootcommunitydevproj/service/UserManagementService.java
@@ -1,6 +1,8 @@
 package com.springbootcommunitydevproj.service;
 
+import com.springbootcommunitydevproj.dto.SetUserToBlockedUserRequest;
 import com.springbootcommunitydevproj.dto.UserManagementInfoDto;
+import com.springbootcommunitydevproj.repository.BlockedUserRepository;
 import com.springbootcommunitydevproj.repository.UserManagementRepository;
 import com.springbootcommunitydevproj.utils.ResponseMessages;
 import java.util.List;
@@ -13,6 +15,7 @@ import org.springframework.stereotype.Service;
 public class UserManagementService {
 
     private final UserManagementRepository userManagementRepository;
+    private final BlockedUserRepository blockedUserRepository;
 
     // 한 페이지 당 30개의 검색 결과가 기준이므로 offset = (page - 1) * 30이 됩니다.
     public List<UserManagementInfoDto> getAllUserManagementInfo(Integer page) {
@@ -37,9 +40,23 @@ public class UserManagementService {
             return ResponseMessages.INVALID_LEVEL;
         }
 
-        Integer result =  userManagementRepository.changeUserLevel(request.get("user_id"), request.get("level"));
+        Integer result = userManagementRepository.changeUserLevel(request.get("user_id"), request.get("level"));
 
         // 쿼리 실행 결과에 따라 성공, 실패 메시지를 반환합니다.
         return result.equals(1) ? ResponseMessages.CHANGE_LEVEL_SUCCESS : ResponseMessages.CHANGE_LEVEL_FAIL;
+    }
+
+    // 특정 회원을 가입 제한으로 설정합니다.
+    public String setUserToBlockedUser(SetUserToBlockedUserRequest request) {
+        // 이미 가입 제한 테이블에 해당 회원이 존재하는지 확인합니다.
+        if (blockedUserRepository.findById(request.getUserId()).isPresent()) {
+            return ResponseMessages.ALREADY_SET_BLOCKED_USER;
+        }
+
+        Integer result = blockedUserRepository.moveUserToBlockedUser(request.getUserId());
+
+        // 쿼리 실행 결과에 따라 성공, 실패 메시지를 반환합니다.
+        return result.equals(1) ? ResponseMessages.SET_USER_TO_BLOCKED_USER_SUCCESS
+            : ResponseMessages.SET_USER_TO_BLOCKED_USER_FAIL;
     }
 }
