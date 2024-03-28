ef9ceacc09e6e1713cf993800a472fa3220ba862
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-03-28 16:29:41 +0900

    Feat : issue#5 UserManagement의 Response용 객체 작성

--- src/main/java/com/springbootcommunitydevproj/dto/UserManagementResponse.java
+++ src/main/java/com/springbootcommunitydevproj/dto/UserManagementResponse.java
@@ -2,14 +2,11 @@ package com.springbootcommunitydevproj.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
-@NoArgsConstructor
-public class SetUserToBlockedUserRequest {
-
-    private Integer userId;
+public class UserManagementResponse {
+    private String result;
 }
