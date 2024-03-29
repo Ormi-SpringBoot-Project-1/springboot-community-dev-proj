37b7193618780773272bd15a5885fd75274dc258
Imlay16 <artisboom@naver.com>
2024-03-29 14:49:10 +0900

    templates added

--- src/main/java/com/springbootcommunitydevproj/controller/PostViewController.java
+++ src/main/java/com/springbootcommunitydevproj/controller/PostViewController.java
@@ -12,13 +12,13 @@ public class PostViewController {
     @GetMapping("/posts")
     public String showAllPosts(Model model) {
 
-        return "View";
+        return "PostList";
     }
 
     // 특정 게시판 글 목록
     @GetMapping("/posts/{board_id}")
     public String showBoardPosts(@PathVariable(name="board_id") Integer board_id, Model model) {
 
-        return "ViewBoard";
+        return "PostBoard";
     }
 }
