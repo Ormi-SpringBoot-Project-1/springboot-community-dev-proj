d4b0486ecc47e24803a6a76ee938ca850661be6f
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-04-02 09:22:49 +0900

    Feat : 게시글 조회 관련 Service 메소드 작성

--- src/main/java/com/springbootcommunitydevproj/service/PostService.java
+++ src/main/java/com/springbootcommunitydevproj/service/PostService.java
@@ -1,6 +1,7 @@
 package com.springbootcommunitydevproj.service;
 
 import com.springbootcommunitydevproj.dto.AddPostRequest;
+import com.springbootcommunitydevproj.dto.PostListDto;
 import com.springbootcommunitydevproj.dto.UpdatePostRequest;
 import com.springbootcommunitydevproj.model.*;
 import com.springbootcommunitydevproj.repository.BoardAuthorityRepository;
@@ -8,6 +9,10 @@ import com.springbootcommunitydevproj.repository.BoardRepository;
 import com.springbootcommunitydevproj.repository.PostAuthorityRepository;
 import com.springbootcommunitydevproj.repository.PostRepository;
 import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import java.util.List;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import java.util.Optional;
@@ -16,16 +21,55 @@ import static com.springbootcommunitydevproj.utils.ResponseMessages.POST_ID_NOT_
 
 @Service
 public class PostService {
+
     private final PostRepository postRepository;
     private final BoardRepository boardRepository;
     private final BoardAuthorityRepository boardAuthorityRepository;
     private final PostAuthorityRepository postAuthorityRepository;
+
     public PostService(PostRepository postRepository, BoardRepository boardRepository,
-                       BoardAuthorityRepository boardAuthorityRepository, PostAuthorityRepository postAuthorityRepository) {
+        BoardAuthorityRepository boardAuthorityRepository, PostAuthorityRepository postAuthorityRepository) {
         this.postRepository = postRepository;
         this.boardRepository = boardRepository;
         this.boardAuthorityRepository = boardAuthorityRepository;
         this.postAuthorityRepository = postAuthorityRepository;
+    }
+
+    /**
+     *      게시판 이름을 통해 해당 게시판에 등록된 게시글 목록을 가져오는 메소드입니다. <br>
+     *      게시판 이름이 없거나 존재하지 않는 게시판이면 자유 게시판 목록을 반환합니다. <br>
+     *      한 페이지 당 10개의 검색 결과가 기준이므로 offset = (page - 1) * 10이 됩니다. <br>
+     *      PageRequest 객체는 offset = pageNumber * pageSize로 계산합니다. <br>
+     *      만약 page가 음수거나 정렬할 열의 이름, 정렬 순서가 올바르지 않다면 postId 기준 오름차순으로 첫 번째 페이지의 목록을 가져옵니다.
+     */
+    public List<PostListDto> getPostListByBoardName(String boardName, String search, Integer page, String orderBy, String sort) {
+        PageRequest pageRequest = setPageRequest(page, orderBy, sort);
+        boardName = convertBoardNameToDBFormat(boardName);
+
+        return postRepository.getPostListByBoardName(boardName, search, pageRequest);
+    }
+
+    /**
+     *      게시판 이름과 회원의 id를 통해 해당 게시판에 회원이 등록한 게시글 목록을 가져오는 메소드입니다. <br>
+     *      게시판 이름이 없거나 존재하지 않는 게시판이면 자유 게시판 목록을 기준으로 합니다. <br>
+     *      한 페이지 당 10개의 검색 결과가 기준이므로 offset = (page - 1) * 10이 됩니다. <br>
+     *      PageRequest 객체는 offset = pageNumber * pageSize로 계산합니다. <br>
+     *      만약 page가 음수거나 정렬할 열의 이름, 정렬 순서가 올바르지 않다면 postId 기준 오름차순으로 첫 번째 페이지의 목록을 가져옵니다.
+     */
+    public List<PostListDto> getPostListByUserId(String boardName, Integer userId, Integer page, String orderBy, String sort) {
+        PageRequest pageRequest = setPageRequest(page, orderBy, sort);
+        boardName = convertBoardNameToDBFormat(boardName);
+
+        return postRepository.getPostListByUserId(boardName, userId, pageRequest);
+    }
+
+    /**
+     *      회원 목록의 총 페이지 수를 반환합니다.
+     */
+    public Integer getPostPages(String boardName) {
+        boardName = convertBoardNameToDBFormat(boardName);
+
+        return (int) Math.ceil((double) postRepository.getCountByBoardName(boardName) / 10);
     }
 
     // 게시글 저장 api
@@ -33,43 +77,45 @@ public class PostService {
 
         // 게시글 구성 요소: 제목, 컨텐츠, 유저, 좋아요, 싫어요, 조회수, 글 권한
         Post post = Post.builder()
-                .title(request.getTitle())
-                .content(request.getContent())
-                .board(boardRepository.findById(board_id).get()) // 게시판
-                .user(request.getUser())
-                .likes(0) // 디폴트 0
-                .dislikes(0) // 디폴트 0
-                .views(0) // 디폴트 0
-                .authority(postAuthorityRepository.findById(auth_id).get()) // 게시글 권한
-                .build();
+            .title(request.getTitle())
+            .content(request.getContent())
+            .board(boardRepository.findById(board_id).get()) // 게시판
+            .user(request.getUser())
+            .views(0) // 디폴트 0
+            .createdAt(LocalDateTime.now())
+            .authority(postAuthorityRepository.findById(auth_id).get()) // 게시글 권한
+            .build();
 
         return postRepository.save(post);
     }
 
     // 상세 게시글 불러오기 api
+    @Transactional
     public Post findById(Integer id) {
+        postRepository.updateViews(id);
+
         return postRepository.findById(id).orElseThrow(() -> new IllegalArgumentException(POST_ID_NOT_FOUND));
     }
 
     // 게시글 수정 api
     @Transactional
-    public Post update(Integer id, UpdatePostRequest request) {
+    public String update(Integer id, UpdatePostRequest request) {
+        if (request.getTitle() == null && request.getContent() == null) {
+            return "게시글 변경 사항이 없습니다.";
+        }
 
-        Post post = findById(id);
+        String title = "";
+        String content = "";
 
-        String title = post.getTitle();
-        String content = post.getContent();
-
-        if (title != request.getTitle()) {
+        if (request.getTitle() != null) {
             title = request.getTitle();
         }
 
-        if (content != request.getContent()) {
+        if (request.getContent() != null) {
             content = request.getContent();
         }
 
-        post.update(title, content);
-        return post;
+        return postRepository.updatePost(title, content, id).equals(1) ? "게시글이 성공적으로 수정되었습니다." : "게시글 수정에 실패했습니다.";
     }
 
     // 게시글 삭제 api
@@ -80,8 +126,44 @@ public class PostService {
             Post post = optionalPost.get();
             // 게시글 삭제
             postRepository.delete(post);
-        } else {
+        }
+        else {
             throw new IllegalArgumentException(POST_ID_NOT_FOUND);
+        }
+    }
+
+    /**
+     *      쿼리로 검색할 수 있게 boardName을 DB에 저장된 값으로 매칭시키는 메소드입니다. <br>
+     *      DB에 지정한 값이 아닌 다른 값이 오면 "자유 게시판"으로 매핑됩니다.
+     */
+    private String convertBoardNameToDBFormat(String boardName) {
+        switch (boardName) {
+            case "attention" -> boardName = "공지 사항";
+            case "recruit" -> boardName = "그룹 모집 게시판";
+            case "evaluation" -> boardName = "평가 게시판";
+            case "share" -> boardName = "정보 공유 게시판";
+            default -> boardName = "자유 게시판";
+        }
+
+        return boardName;
+    }
+
+    /**
+     *      정렬 방식, 정렬 기준, 페이징을 위한 PageRequest 객체를 세팅하는 메소드입니다.
+     */
+    private PageRequest setPageRequest(Integer page, String orderBy, String sort) {
+        if (!(orderBy.equals("postId") || orderBy.equals("author")
+            || orderBy.equals("date") || orderBy.equals("views") || orderBy.equals("likes")
+            || sort.equals("asc") || sort.equals("desc"))
+            || page < 0) {
+            return PageRequest.of(0, 10, Direction.ASC, "postId");
+        }
+
+        if (sort.equals("asc")) {
+            return PageRequest.of(page - 1, 10, Direction.ASC, orderBy);
+        }
+        else {
+            return PageRequest.of(page - 1, 10, Direction.DESC, orderBy);
         }
     }
 }
