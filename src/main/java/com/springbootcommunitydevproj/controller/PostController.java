68bec830c3ba1941509b83e41b0be51f801d8238
JinhwanJung-0125 <joung.jinhawn@gmail.com>
2024-04-02 17:21:37 +0900

    Feat : 게시글 생성 Controller API 수정

--- src/main/java/com/springbootcommunitydevproj/controller/PostController.java
+++ src/main/java/com/springbootcommunitydevproj/controller/PostController.java
@@ -1,14 +1,17 @@
 package com.springbootcommunitydevproj.controller;
 
-import com.springbootcommunitydevproj.dto.AddPostRequest;
+import com.springbootcommunitydevproj.dto.PostRequest;
 import com.springbootcommunitydevproj.dto.PostResponse;
 import com.springbootcommunitydevproj.dto.UpdatePostRequest;
-import com.springbootcommunitydevproj.model.Post;
+import com.springbootcommunitydevproj.model.User;
 import com.springbootcommunitydevproj.service.PostService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
 public class PostController {
     private final PostService postService;
@@ -17,9 +20,20 @@ public class PostController {
         this.postService = postService;
     }
 
-    @PostMapping("/api/post") // 게시글 생성 요청
-    public ResponseEntity<PostResponse> savePost(@RequestParam Integer board_id, @RequestParam Integer auth_id, @RequestBody AddPostRequest request) {
-        return ResponseEntity.status(HttpStatus.CREATED).body(postService.savePost(board_id, auth_id, request).toResponse());
+    @PostMapping("/api/post/{boardName}") // 게시글 생성 요청
+    public ResponseEntity<String> savePost(
+        @PathVariable(name = "boardName") String boardName,
+        @ModelAttribute PostRequest newPost,
+        @AuthenticationPrincipal User user) {
+
+        try {
+            postService.savePost(boardName, newPost, user);
+        } catch (IllegalArgumentException e) {
+            log.error("Board Name 불일치");
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("존재하지 않는 게시판에 게시글 작성을 시도하였습니다.");
+        }
+
+        return ResponseEntity.status(HttpStatus.CREATED).body("게시글이 성공적으로 작성되었습니다.");
     }
 
     @GetMapping("/api/post/{post_id}") // 게시글 조회
