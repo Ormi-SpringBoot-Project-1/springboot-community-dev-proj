06484b26cf4ba9b0b5a6a1fd56aec019fa106092
sjw0851 <106232058+sjw0851@users.noreply.github.com>
2024-04-04 17:17:46 +0900

    Comment, User Entity 수정 및 로그인실패 헨들러 작성, repository, service, controller 생성 및 수정

---
+++ src/main/java/com/springbootcommunitydevproj/config/SwaggerConfig.java
@@ -0,0 +1,26 @@
+package com.springbootcommunitydevproj.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("Community Dev Project API")
+                .description("스프링부트 커뮤니티 제작 프로젝트의 API 문서입니다.")
+                .version("1.0.0");
+    }
+
+}
---
+++ src/test/SpringbootCommunityDevProjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootcommunitydevproj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootCommunityDevProjApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
